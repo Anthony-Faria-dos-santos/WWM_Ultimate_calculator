6ea6a043bc36d37de6b839ff33354eba348f5a15
Anthony <anthony.devfsjs@gmail.com>
2026-03-03 22:42:33 +0100

    feat(hooks): add barrel export

---
+++ src/lib/hooks/index.ts
@@ -0,0 +1,20 @@
+/**
+ * Barrel export des hooks React Query.
+ */
+
+export {
+  useCalculateDamage,
+  useCalculateExpected,
+  useCalculateRotation,
+  useCalculateGraduation,
+  useCalculateCompare,
+  useCalculateMarginal,
+} from './useCalculate';
+
+export {
+  useBuilds,
+  useBuild,
+  useCreateBuild,
+  useUpdateBuild,
+  useDeleteBuild,
+} from './useBuilds';
